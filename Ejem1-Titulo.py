1d3048d7bddcadf6681d141b884917503c1ad061
Felipe-rodriguez-Cont <felipehugo.1234@gmail.com>
2023-09-03 15:21:18 -0300

    Update Ejem1-Titulo.py

--- Ejem1-Titulo.py
+++ Ejem1-Titulo.py
@@ -2,19 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-
-chrome_options = Options()
-chrome_options.add_argument("--ignore-certificate-errors")
-chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
+options = Options()
+options.add_argument('--ignore-certificate-errors')
 #ubicacion del driver.
 driver = webdriver.Chrome(executable_path='C:\\Users\\x\\Documents\\chromedriver.exe')
 
 #pagina a trabajar
 driver.get("https://demoqa.com/")
-
-#ELIMINAR REGISTRO DE ERRORES EN CONSOLA
-
 
 print ("TITULO DE PAGINA: ",driver.title)
 driver.close
